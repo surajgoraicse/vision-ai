c86dfd80559e0038b2918e6b9d462c722e6dfc8c
surajgoraicse <surajgoraicse@gmail.com>
2025-03-05 12:47:14 +0530

    shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from "./components/ui/button";
 
 function App() {
-  return (
-	<div className="text-3xl">App</div>
-  )
+	return (
+		<>
+			<div className="text-3xl">
+				<Button>Click me</Button>
+			</div>
+
+		</>
+	);
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { crx } from '@crxjs/vite-plugin';
-import manifest from './manifest.json';
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    crx({ manifest })
-  ]
+	plugins: [tailwindcss(), react(), crx({ manifest })],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
